f82242947f041a6d1b0a10050d3380e769102708
ScottGrizzell <scottindiegrizzell@gmail.com>
2026-07-19 22:47:48 -0700

    reverting naming change of route tables

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -75,12 +75,12 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
-resource "aws_route_table_association" "public_1" {
+resource "aws_route_table_association" "public_1_assoc" {
   subnet_id      = aws_subnet.public_1.id
   route_table_id = aws_route_table.public_rt.id
 }
 
-resource "aws_route_table_association" "public_2" {
+resource "aws_route_table_association" "public_2_assoc" {
   subnet_id      = aws_subnet.public_2.id
   route_table_id = aws_route_table.public_rt.id
 }
